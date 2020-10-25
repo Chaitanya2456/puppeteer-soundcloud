903d2d4b7d810ae1326454253a718e2df141636a
Chaitanya Reddy <chaitanya.reddy@go-jek.com>
2020-10-25 23:21:29 +0530

    Scrape available genres and print to console

--- soundclound_streamer.js
+++ soundclound_streamer.js
@@ -3,6 +3,7 @@ const puppeteer = require('puppeteer-core');
 (async() => {
 
 const MUSIC_URL = 'https://soundcloud.com/discover';
+const genreSelector = '.mixedModularHome__item';
 
 const browser = await puppeteer.launch({
     headless: true,
@@ -17,6 +18,21 @@ const browser = await puppeteer.launch({
 
 const page = await browser.newPage();
 
-await page.goto(MUSIC_URL, { waitUntil: 'networkidle0' })
+await page.goto(MUSIC_URL, { waitUntil: 'networkidle0' });
+
+var genreNamesList = await page.evaluate((genreSelector) => {
+    var genreNamesList = [];
+    const listOfAllGenres = Array.from(document.querySelectorAll(genreSelector));
+    for (index in listOfAllGenres) {
+      genreNamesList.push(listOfAllGenres[index].children[0].children[0].children[0].innerText);
+    }
+    return genreNamesList;
+}, genreSelector);
+  
+console.log("\n" + "Available genres are : " + "\n");
+
+for (index in genreNamesList) {
+    console.log((parseInt(index) + 1) + ' - ' + genreNamesList[index]);
+}
 
 })();
